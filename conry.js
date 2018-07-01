7ccb6970e5eafabf7fd33f9b4adaa4e158637f9c
pronapro <nakirandaproscovia@gmail.com>
2018-07-01 03:03:29 -0700

    Update conry.js

--- conry.js
+++ conry.js
@@ -1,13 +1,13 @@
 let from= document.getElementById('from');
 
 let to= document.getElementById('to');
-const URL_country = ' https://free.currencyconverterapi.com/api/v5/countries';
+const URL_country = ' https://free.currencyconverterapi.com/api/v5/currencies';
 
 
 function load(){
 
 let le;
-let ne;
+
 fetch(URL_country)
 .then(function(response) {
   console.log(response.ok)
@@ -15,15 +15,15 @@ fetch(URL_country)
   return bodyType;
 })
 .then(function(jsonObj) {
-  // console.log(jsonObj.results)
+ console.log(jsonObj.results)
   let currencies = jsonObj.results
 for(let i in currencies) {
-  console.log(i)
-   le +=`<option> ${i} <option>`
-   ne += `<option> ${i} <option>`
+ //console.log(i)
+   le +=`<option> ${i} <option>`;
+  
   
 }
- to.innerHTM = ne;
+to.innerHTML =le;
  from.innerHTML =le;
 
 })
@@ -31,19 +31,40 @@ for(let i in currencies) {
 
 }
 
-function conver(){
 
-	fetch(URL_country)
+
+
+function conver()
+{
+	if (document.getElementById('amount') != null) {
+    
+     let am = parseInt(document.getElementById('amount')).value;
+}
+	 let amount = parseInt(document.getElementById('amount').value);
+  const fromCurr = from.options[from.selectedIndex].text;
+
+  const toCurr =to.options[to.selectedIndex].text;
+
+ // console.log(fromCurr)
+ //  console.log(toCurr)
+const query = `${fromCurr}_${toCurr}`;
+  const url = `https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=y`;
+	fetch(url)
 .then(function(response) {
-  console.log(response.ok)
-  const bodyType = response.json();
-  return bodyType;
+	//console.log(response.json())
+  
+  return  response.json();
 })
 .then(function(jsonObj) {
-  // console.log(jsonObj.results)
-  let currencies = jsonObj.results
 
+  let rate =jsonObj[query].val;
+
+  result  = rate * amount;
+  console.log(result );
+ 
+  document.getElementById('res').innerHTML = `<p> ${result} <p>`;
 
 
 })
 }
+
